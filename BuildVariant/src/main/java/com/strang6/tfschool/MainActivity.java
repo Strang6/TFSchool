d627db6abe07a80148223ae145548940f3e759f4
Strang6 <dariagorod@gmail.com>
2017-10-02 23:14:44 +0300

    Initial commit

---
+++ BuildVariant/src/main/java/com/strang6/tfschool/MainActivity.java
@@ -0,0 +1,13 @@
+package com.strang6.tfschool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
